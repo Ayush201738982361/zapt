3154d1b8b194343885423e35a08a9084a211181d
Ayush201738982361 <ayushnair87@gmail.com>
2025-10-10 00:35:26 +0530

    Button component

---
+++ client/src/Components/Button.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { ButtonContext } from "../Contexts/ButtonContext";
+
+function Button({ text }) {
+  const ButtonText = useContext(ButtonContext);
+  const button = text || ButtonText;
+  return (
+    <>
+      <button
+        type="button"
+        className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+      >
+        {button}
+      </button>
+    </>
+  );
+}
+
+export default Button;
