8325e92460b1f04e126371fa5ed2fc1c0f39ad6d
Ayush201738982361 <ayushnair87@gmail.com>
2025-10-11 11:17:51 +0530

    Category component

---
+++ client/src/Components/Categories.jsx
@@ -0,0 +1,13 @@
+function Categories() {
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center h-[50vh] bg-black text-center -mt-10">
+        <h3 className="text-4xl sm:text-6xl font-extrabold text-white tracking-tight">
+          Practice Aptitude Questions Sorted By Category.
+        </h3>
+      </section>
+    </>
+  );
+}
+
+export default Categories;
