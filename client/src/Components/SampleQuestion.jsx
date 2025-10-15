d662cd325b7bb0e353b3bab02ab06bb677d6822d
Ayush201738982361 <ayushnair87@gmail.com>
2025-10-15 21:38:31 +0530

    Sample questions component

---
+++ client/src/Components/SampleQuestion.jsx
@@ -0,0 +1,19 @@
+function SampleQuestion() {
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center bg-black text-center mt-40 mb-20">
+        <h1 className="text-4xl sm:text-6xl font-extrabold text-white tracking-tight">
+          Here are few challanges for you get your feet wet
+        </h1>
+      </section>
+      <div>
+        <p className="flex flex-col items-center justify-center mb-5">
+          A sum of money at simple interest amounts to Rs. 815 in 3 years and to
+          Rs. 854 in 4 years. The sum is:
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default SampleQuestion;
