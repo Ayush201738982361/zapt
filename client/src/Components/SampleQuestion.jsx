f63ef54120b081c94c1ff19e8a72741c92f67efc
Ayush201738982361 <ayushnair87@gmail.com>
2025-10-17 12:01:24 +0530

    Completed sample question section

--- client/src/Components/SampleQuestion.jsx
+++ client/src/Components/SampleQuestion.jsx
@@ -1,16 +1,59 @@
 function SampleQuestion() {
   return (
     <>
-      <section className="flex flex-col items-center justify-center bg-black text-center mt-40 mb-20">
+      <section className="flex flex-col items-center justify-center bg-black text-center mt-40 mb-20 px-4">
         <h1 className="text-4xl sm:text-6xl font-extrabold text-white tracking-tight">
-          Here are few challanges for you get your feet wet
+          Here are a few challenges to get your feet wet
         </h1>
       </section>
-      <div>
-        <p className="flex flex-col items-center justify-center mb-5">
+
+      <div className="flex flex-col items-center justify-center">
+        <p className="mb-5 text-center text-lg max-w-xl">
           A sum of money at simple interest amounts to Rs. 815 in 3 years and to
           Rs. 854 in 4 years. The sum is:
         </p>
+
+        <div className="flex flex-col gap-3">
+          <label className="flex items-center gap-2">
+            <input
+              type="radio"
+              id="opt1"
+              name="radio-2"
+              className="radio radio-sm"
+            />
+            Rs. 650
+          </label>
+
+          <label className="flex items-center gap-2">
+            <input
+              type="radio"
+              id="opt2"
+              name="radio-2"
+              className="radio radio-sm"
+            />
+            Rs. 690
+          </label>
+
+          <label className="flex items-center gap-2">
+            <input
+              type="radio"
+              id="opt3"
+              name="radio-2"
+              className="radio radio-sm"
+            />
+            Rs. 698
+          </label>
+
+          <label className="flex items-center gap-2">
+            <input
+              type="radio"
+              id="opt4"
+              name="radio-2"
+              className="radio radio-sm"
+            />
+            Rs. 700
+          </label>
+        </div>
       </div>
     </>
   );
