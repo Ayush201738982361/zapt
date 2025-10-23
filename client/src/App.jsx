85ed4557e99c99204dec43559cf082d5607e5287
Ayush201738982361 <ayushnair87@gmail.com>
2025-10-23 20:05:24 +0530

    Imported provider in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { ButtonContext } from "./Contexts/ButtonContext";
+
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 
 function App() {
+  const [ButtonText] = useState("Default");
   return (
     <>
-      <Home />
+      <ButtonContext.Provider value={{ ButtonText }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </ButtonContext.Provider>
     </>
   );
 }
