c342a39e0d0dedc82fc3914e00cf9fba797370c1
Ayush201738982361 <ayushnair87@gmail.com>
2025-10-23 20:04:42 +0530

    Fixed key error for map function

--- client/src/Components/Categories.jsx
+++ client/src/Components/Categories.jsx
@@ -40,33 +40,29 @@ function Categories() {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 px-6 pb-16">
         {categories.map((x, i) => {
           return (
-            <>
-              <div
-                key={i}
-                className="flex flex-col bg-white rounded-2xl max-w-sm mx-auto shadow-2xl hover:shadow-2xl hover:shadow-blue-500/20 transition-all duration-300"
-              >
-                <div className="px-6 py-8 sm:p-10 sm:pb-6">
-                  <div className="grid items-center justify-center grid-cols-1 text-center">
-                    <div className="flex justify-center mb-4 text-5xl text-purple-600">
-                      {x.icon}
-                    </div>
-                    <div>
-                      <h2 className="text-lg font-medium tracking-tighter text-gray-900 lg:text-3xl">
-                        {x.heading}
-                      </h2>
-                      <p className="mt-4 text-gray-600 text-sm">
-                        {x.subheading}
-                      </p>
-                    </div>
+            <div
+              key={i}
+              className="flex flex-col bg-white rounded-2xl max-w-sm mx-auto shadow-2xl hover:shadow-2xl hover:shadow-blue-500/20 transition-all duration-300"
+            >
+              <div className="px-6 py-8 sm:p-10 sm:pb-6">
+                <div className="grid items-center justify-center grid-cols-1 text-center">
+                  <div className="flex justify-center mb-4 text-5xl text-purple-600">
+                    {x.icon}
+                  </div>
+                  <div>
+                    <h2 className="text-lg font-medium tracking-tighter text-gray-900 lg:text-3xl">
+                      {x.heading}
+                    </h2>
+                    <p className="mt-4 text-gray-600 text-sm">{x.subheading}</p>
                   </div>
                 </div>
-                <div className="flex justify-center px-6 pb-8 sm:px-8">
-                  <a className="mt-4 text-purple-900 text-sm" href="#">
-                    Start
-                  </a>
-                </div>
               </div>
-            </>
+              <div className="flex justify-center px-6 pb-8 sm:px-8">
+                <a className="mt-4 text-purple-900 text-sm" href="#">
+                  Start
+                </a>
+              </div>
+            </div>
           );
         })}
       </div>
