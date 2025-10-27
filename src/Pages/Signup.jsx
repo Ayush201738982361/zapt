59861931983bf0b895e10e5211575869d68af2a2
Ayush201738982361 <ayushnair87@gmail.com>
2025-10-27 11:24:13 +0530

    Login and Signup components

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,23 +1,34 @@
-import Button from "../Components/Button";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
-import { Link } from "react-router-dom";
+import Button from "../Components/Button";
 
-function Login() {
+function Signup() {
   return (
     <>
       <Navbar />
-
       <section className="mt-20">
         <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-8">
           <div className="xl:mx-auto xl:w-full shadow-md p-4 xl:max-w-sm 2xl:max-w-md">
             <div className="mb-2 flex justify-center"></div>
             <h2 className="text-center text-2xl font-bold leading-tight text-white">
-              Login to your account
+              Create a account for free
             </h2>
 
             <form className="mt-8" method="POST" action="#">
               <div className="space-y-5">
+                <div>
+                  <label className="text-base font-medium text-purple-500">
+                    Username
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      placeholder="Username"
+                      type="username"
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    />
+                  </div>
+                </div>
+
                 <div>
                   <label className="text-base font-medium text-purple-500">
                     Email address
@@ -45,18 +56,10 @@ function Login() {
                   </div>
                 </div>
                 <div className="mt-2 flex item-center justify-center">
-                  <Button text={"Get started"} targetLink={"#"} />
+                  <Button text={"Create account"} targetLink={"#"} />
                 </div>
               </div>
             </form>
-            <div className="mt-3 space-y-3">
-              <Link
-                to="/signup"
-                className="mt-2 flex item-center justify-center text-sm text-white"
-              >
-                Don't have an account? Create a free account
-              </Link>
-            </div>
           </div>
         </div>
       </section>
@@ -65,4 +68,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Signup;
