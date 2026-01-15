9714492af132532ab8816649dd8ed907361d4d0c
Ayush201738982361 <ayushnair87@gmail.com>
2026-01-15 14:11:39 +0530

    Admin page

---
+++ src/Pages/Admin.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Components/Navbar";
+// import { Toaster } from "react-hot-toast";
+
+function Admin() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default Admin;
