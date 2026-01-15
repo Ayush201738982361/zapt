a0d04fefd63585b91270d1711947044505044f7c
Ayush201738982361 <ayushnair87@gmail.com>
2026-01-15 23:45:11 +0530

    Terminal sanity check

--- src/App.jsx
+++ src/App.jsx
@@ -30,9 +30,9 @@ function App() {
 
   if (loading) {
     return (
-      <div class="text-center mt-5">
-        <div class="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-yellow-500 mx-auto"></div>
-        <h2 class="text-zinc-900 dark:text-white mt-4">Loading...</h2>
+      <div className="text-center mt-5">
+        <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-yellow-500 mx-auto"></div>
+        <h2 className="text-zinc-900 dark:text-white mt-4">Loading...</h2>
       </div>
     );
   }
