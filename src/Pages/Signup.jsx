7e4c4f91bbd47db483f31d5856eabf897eedbfd8
Ayush201738982361 <ayushnair87@gmail.com>
2026-01-18 12:39:58 +0530

    type fix

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -62,7 +62,7 @@ function Signup() {
                         setUsername(e.target.value);
                       }}
                       placeholder="Username"
-                      type="username"
+                      type="text"
                       className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                     />
                   </div>
