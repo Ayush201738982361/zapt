1bf0423d1ed12cceee302f472b01c32b41b310a9
Ayush201738982361 <ayushnair87@gmail.com>
2026-01-26 22:22:44 +0530

    Logical Reasoning

--- src/Pages/LogicalReasoning.jsx
+++ src/Pages/LogicalReasoning.jsx
@@ -1,9 +1,10 @@
 import { supabase } from "../supabase-client";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 import { useEffect, useState } from "react";
 
 function LogicalReasoning() {
-  const [questionTitle, setQuestionTitle] = useState([]);
-  const [difficulty, setDifficulty] = useState([]);
+  const [questionData, setData] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -12,21 +13,23 @@ function LogicalReasoning() {
         .select("question_title,difficulty");
 
       if (error) {
+        // Add toast later
         console.log("Error in fetching data:", error);
       }
 
-      setQuestionTitle(data.map((item) => item.question_title));
-
-      setDifficulty(data.map((item) => item.difficulty));
+      setData(data);
     }
-
-    console.log(questionTitle);
 
     fetchData();
   }, []);
 
+  // console.log(questionData);
+
   return (
     <div className="w-full p-4">
+      <div className="mb-10">
+        <Navbar />
+      </div>
       <div className="relative flex flex-col mx-auto max-w-2x2 overflow-hidden text-white bg-black shadow-md rounded-xl bg-clip-border border border-slate-800">
         <table className="w-full text-left table-auto">
           <thead>
@@ -52,17 +55,23 @@ function LogicalReasoning() {
             <tr className="hover:bg-slate-800/30 transition-colors">
               <td className="p-3 border-b border-slate-800">
                 <p className="block font-sans text-sm antialiased font-normal leading-normal text-slate-200">
-                  Dummy Data
+                  <a href="#">
+                    {questionData.map((x) => {
+                      return x.question_title;
+                    })}
+                  </a>
                 </p>
               </td>
               <td className="p-3 border-b border-slate-800">
                 <p className="block font-sans text-sm antialiased font-normal leading-normal text-slate-200">
-                  Dummy Data
+                  {questionData.map((x) => {
+                    return x.difficulty;
+                  })}
                 </p>
               </td>
               <td className="p-3 border-b border-slate-800">
                 <p className="block font-sans text-sm antialiased font-normal leading-normal text-slate-200">
-                  Dummy Data
+                  Solved/Unsolved
                 </p>
               </td>
             </tr>
