311dc018bce217384edc2001672e3622e72715fd
Ayush201738982361 <ayushnair87@gmail.com>
2026-01-26 22:28:03 +0530

    Prod url test

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,10 @@ function App() {
             <Route path="/index.html" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/logical-reasoning" element={<LogicalReasoning />} />
+            <Route
+              path="/logical-reasoning.html"
+              element={<LogicalReasoning />}
+            />
             <Route
               path="/admin"
               element={
